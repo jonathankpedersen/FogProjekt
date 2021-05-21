d6a90086bdb0a3bccc821f78fae813edc8243b38
albiek1 <54135401+albiek1@users.noreply.github.com>
2021-05-21 02:09:38 +0200

    more calculations done. will finish tomorrow when my brain works

--- src/main/java/web/commands/CalcOrderCommand.java
+++ src/main/java/web/commands/CalcOrderCommand.java
@@ -15,11 +15,23 @@ public class CalcOrderCommand extends CommandProtectedPage{
         Double width = Double.parseDouble(request.getParameter("width"));
         Double length = Double.parseDouble(request.getParameter("length"));
         Double numBeams = 0.0;
+        Double posts = 0.0;
         if(Math.ceil(length/55)+1 > Math.ceil(length/60)+1){
             numBeams = Math.ceil(length/60)+1;
         }else{
             numBeams = Math.ceil(length/55)+1;
         }
+
+        if(length < 6.0){
+            posts = 4.0;
+        }else{
+            posts = 6.0;
+        }
+
+        if(width > 6.0){
+            posts = posts*1.5;
+        }
+
         Double uni190mm = numBeams*2; //total mængde, unviversal 190mm beslag har både højre er forskellig fra venstre
         Double beslagSkruer = Math.ceil((uni190mm*9)/250);
         return pageToShow;
